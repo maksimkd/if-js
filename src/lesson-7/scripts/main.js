cd45ed9164159d849e3c855db3f8143b6c4059dd
maksim <maksimkot.work@gmail.com>
2023-06-28 17:32:18 +0300

    udate getCalendarMonth function

--- src/lesson-7/scripts/main.js
+++ src/lesson-7/scripts/main.js
@@ -57,3 +57,39 @@ const deepEqual = (object1, object2) => {
 
 console.log(deepEqual(obj1, obj2)); // true
 console.log(deepEqual(obj1, obj3)); // false
+
+//===========================================================
+
+const daysInMonth = 30;
+const daysInWeek = 7;
+const dayOfWeek = 4;
+const getCalendarMonth = (
+  daysInMonth,
+  daysInWeek,
+  dayOfWeek,
+  checkInDate,
+  checkOutDate,
+) => {
+  const result = [];
+  const weekValue = Math.ceil(daysInMonth / daysInWeek);
+  let start = daysInMonth - dayOfWeek + 1;
+
+  for (let i = 0; i < weekValue; i++) {
+    const week = [];
+    for (let j = 0; j < daysInWeek; j++) {
+      if (start > daysInMonth) {
+        start = 1;
+      }
+      week.push({
+        dayOfMonth: start,
+        notCurrentMonth: false,
+        selectedDay: (start >= checkInDate && start <= checkOutDate) || false,
+      });
+      start++;
+    }
+    result.push(week);
+  }
+  return result;
+};
+
+console.log(getCalendarMonth(daysInMonth, daysInWeek, dayOfWeek, 5, 15));
